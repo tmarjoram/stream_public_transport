3995326f7cb7fe250468a60d707ddd6fcc11914b
tmarjoram <tom.marjoram@gmail.com>
2021-02-21 17:18:25 +0000

    station producer

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -5,10 +5,14 @@ import time
 
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
-from confluent_kafka.avro import AvroProducer
+from confluent_kafka.avro import AvroProducer, CachedSchemaRegistryClient
 
 logger = logging.getLogger(__name__)
 
+
+HOSTNAME = 'DESKTOP-MRAIJ7J'
+BROKER_URL = f"PLAINTEXT://{HOSTNAME}:9092"
+SCHEMA_REGISTRY_URL = f"http://{HOSTNAME}:8081"
 
 class Producer:
     """Defines and provides common functionality amongst Producers"""
@@ -38,7 +42,7 @@ class Producer:
         #
         #
         self.broker_properties = {
-            # TODO
+            'bootstrap.servers': BROKER_URL,
             # TODO
             # TODO
         }
@@ -49,18 +53,77 @@ class Producer:
             Producer.existing_topics.add(self.topic_name)
 
         # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(self.broker_properties,
+                                     schema_registry=  CachedSchemaRegistryClient(SCHEMA_REGISTRY_URL)
+        )
+
+    @staticmethod
+    def topic_exists(client, topic_name):
+
+        topics = client.list_topics()
+
+        return topic_name in topics.topics
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
+
+        client = AdminClient({"bootstrap.servers": BROKER_URL})
+
         #
         #
         # TODO: Write code that creates the topic for this producer if it does not already exist on
         # the Kafka Broker.
         #
         #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        if not self.topic_exists(client, self.topic_name) :
+
+            """Creates the topic with the given topic name"""
+            # Create the topic. Make sure to set the topic name, the number of partitions, the
+            # replication factor. Additionally, set the config to have a cleanup policy of delete, a
+            # compression type of lz4, delete retention milliseconds of 2 seconds, and a file delete delay
+            # milliseconds of 2 second.
+            #
+            # See: https://docs.confluent.io/current/clients/confluent-kafka-python/#confluent_kafka.admin.NewTopic
+            # See: https://docs.confluent.io/current/installation/configuration/topic-configs.html
+            futures = client.create_topics(
+                [
+                    # SEE http://kafka.apache.org/documentation.html#adminapi
+                    # NewTopic(topic=topic_name,
+                    #         num_partitions=5,
+                    #         replication_factor=1,
+                    #         config={
+                    #             "cleanup.policy":"compact",
+                    #             "compression.type":"lz4",
+                    #             "delete.retention.ms":1*1000,
+                    #             "file.delete.delay.ms":1*1000
+                    #         }
+                    # )
+                    NewTopic(
+                        topic=self.topic_name,
+                        num_partitions=self.num_partitions,
+                        replication_factor=self.num_replicas,
+                        config={
+                            # TODO TM What to set here ? ****
+                            #  *** compact doesn't work - presume because no key **
+                            "cleanup.policy": "delete",
+                            "compression.type": "lz4",
+                            "delete.retention.ms": "2000",
+                            "file.delete.delay.ms": "2000",
+                        },
+                    )
+
+                ]
+            )
+
+            for topic, future in futures.items():
+                try:
+                    future.result()
+                    logger.info(f"topic {self.topic_name} created")
+                except Exception as e:
+                    logger.fatal(f"failed to create topic {self.topic_name}: {e}")
+                    raise
+
+        # logger.info("topic creation kafka integration incomplete - skipping")
 
     def time_millis(self):
         return int(round(time.time() * 1000))
@@ -72,7 +135,11 @@ class Producer:
         # TODO: Write cleanup code for the Producer here
         #
         #
-        logger.info("producer close incomplete - skipping")
+        # TODO TM Anything else ?  ****
+        self.producer.flush(30)
+
+
+        # logger.info("producer close incomplete - skipping")
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
--- producers/models/station.py
+++ producers/models/station.py
@@ -1,5 +1,6 @@
 """Methods pertaining to loading and configuring CTA "L" station data."""
 import logging
+from dataclasses import asdict, dataclass
 from pathlib import Path
 
 from confluent_kafka import avro
@@ -10,6 +11,13 @@ from models.producer import Producer
 
 logger = logging.getLogger(__name__)
 
+@dataclass
+class Arrival():
+    station_id : int
+    train_id: str
+    direction: str
+    prev_station_id : int
+    prev_direction : str
 
 class Station(Producer):
     """Defines a single station"""
@@ -19,7 +27,7 @@ class Station(Producer):
     #
     # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
     #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
         self.name = name
@@ -37,13 +45,13 @@ class Station(Producer):
         # replicas
         #
         #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        topic_name = f"com.udacity.arrival.{station_name}" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
+            num_partitions=5,
+            num_replicas=1,
         )
 
         self.station_id = int(station_id)
@@ -62,18 +70,14 @@ class Station(Producer):
         # TODO: Complete this function by producing an arrival message to Kafka
         #
         #
-        logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        # logger.info("arrival kafka integration incomplete - skipping")
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            key_schema=Station.key_schema,
+            value=asdict(Arrival(self.station_id, train.train_id, direction, prev_station_id, prev_direction)),
+            value_schema=Station.value_schema
+        )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
