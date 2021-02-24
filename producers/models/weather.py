f9a3bfb49c5457bee41dd2ab5385dcc9e1cdc840
tmarjoram <tom.marjoram@gmail.com>
2021-02-24 22:36:52 +0000

    Working Weather Producer - REST Proxy

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -29,26 +29,9 @@ class Weather(Producer):
 
     winter_months = set((0, 1, 2, 3, 10, 11))
     summer_months = set((6, 7, 8))
+    topic_name = f"com.udacity.weather"  # TODO: Come up with a better topic name
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        super().__init__(
-            "weather", # TODO: Come up with a better topic name
-            key_schema=Weather.key_schema,
-            value_schema=Weather.value_schema,
-        )
-
-        self.status = Weather.status.sunny
-        self.temp = 70.0
-        if month in Weather.winter_months:
-            self.temp = 40.0
-        elif month in Weather.summer_months:
-            self.temp = 85.0
 
         if Weather.key_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
@@ -60,6 +43,29 @@ class Weather(Producer):
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
                 Weather.value_schema = json.load(f)
+
+        #
+        #
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
+        # replicas
+        #
+        #
+        super().__init__(
+            self.topic_name, # TODO: Come up with a better topic name
+            key_schema=Weather.key_schema,
+            value_schema=Weather.value_schema,
+            num_partitions=5,
+            num_replicas=1,
+        )
+
+        self.status = Weather.status.sunny
+        self.temp = 70.0
+        if month in Weather.winter_months:
+            self.temp = 40.0
+        elif month in Weather.summer_months:
+            self.temp = 85.0
+
+
 
     def _set_weather(self, month):
         """Returns the current weather"""
@@ -80,31 +86,34 @@ class Weather(Producer):
         # specify the Avro schemas and verify that you are using the correct Content-Type header.
         #
         #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        resp = requests.post(
+           #
+           #
+           # TODO: What URL should be POSTed to?
+           #
+           #
+           f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+           #
+           #
+           # TODO: What Headers need to bet set?
+           #
+           #
+        headers={"Content-Type": "application/vnd.kafka.avro.v2+json",  "Accept": "application/vnd.kafka.v2+json"},
+        data=json.dumps(
+                {
+                    "key_schema": json.dumps(Weather.key_schema),
+                    "value_schema": json.dumps(Weather.value_schema),
+                    "records": [{"key": {"timestamp": self.time_millis()}, "value": {"temperature":self.temp, "status": self.status}}]
+                }
+           ),
+        )
+        try:
+            resp.raise_for_status()
+        except:
+            print(f"Failed to send data to REST Proxy {json.dumps(resp.json(), indent=2)}")
+            raise
+        print(f"Sent data to REST Proxy {json.dumps(resp.json(), indent=2)}")
+
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
